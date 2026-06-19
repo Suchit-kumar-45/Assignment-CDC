ea37a993e8c6695d64e3288ae0f341ac285159cc
Suchit Kumar <suchit844118@gmail.com>
2026-06-19 22:01:22 +0530

    Time: 17 ms (53.33%) | Memory: 28.6 MB (20.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,11 +4,24 @@ public:
         int n=strs.size();
         unordered_map<string,vector<string>>freq;
         vector<vector<string>>ans;
+        
         for(int i=0;i<n;i++)
         {
+            vector<int>arr(26,0);
             string word=strs[i];
-            sort(word.begin(),word.end());
-            freq[word].push_back(strs[i]);
+            for(int j=0;j<word.size();j++)
+            {
+                arr[word[j]-'a']++;
+            }
+            string newWord="";
+            for(int i=0;i<26;i++)
+            {
+                if(arr[i]>0)
+                {
+                    newWord+=string(arr[i],i+'a');
+                }
+            }
+            freq[newWord].push_back(word);
 
 
         }
