1d116c8632b6895225182f3ca26bc17e551ec91e
Suchit Kumar <suchit844118@gmail.com>
2026-08-02 15:28:32 +0530

    Time: 1 ms (14.68%) | Memory: 8.8 MB (22.34%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,24 +1,25 @@
 class Solution {
-public:
-    int solve(int n,vector<int>&dp)
+int solve(int n,vector<int>&dp)
+{
+    int ans=INT_MIN;
+    if(n==1)
     {
-        if(n==1)
-            return 1;
-
-        if(dp[n]!=-1)
-            return dp[n];
-        for(int i=1;i<n;i++)
-        {
-            int nobreak=i*(n-i);
-            int withbreak=i*solve(n-i,dp);
-            dp[n]=max({dp[n],nobreak,withbreak});
-        }
+        return 1;
+    }
+    if(dp[n] != -1)
+    {
         return dp[n];
     }
+    for(int i=1;i<n;i++)
+    {
+        int maxi=i*max(n-i,solve(n-i,dp));
+        ans=max(ans,maxi);
+    }
+    return dp[n]=ans;
+}
+public:
     int integerBreak(int n) {
         vector<int>dp(n+1,-1);
         return solve(n,dp);
-
-        
     }
 };
