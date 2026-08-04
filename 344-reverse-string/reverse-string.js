0f7c41210bca4596d8c7f826309d3456c4b7d35a
Suchit Kumar <suchit844118@gmail.com>
2026-08-05 00:58:48 +0530

    Time: 0 ms (100.00%) | Memory: 61.5 MB (50.56%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let n=s.length;
+    let i=0,j=n-1;
+    while(i<=j)
+    {
+        let temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+
+    }
+    return s;
+    
+};
